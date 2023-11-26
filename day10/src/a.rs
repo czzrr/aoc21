4d0662eaef5c7988ff9999d23929ae7f2af7a01f
Christopher Acosta <chriz@live.dk>
2023-11-26 23:34:46 +0900

    day 10

--- day10/src/a.rs
+++ day10/src/a.rs
@@ -1,3 +1,31 @@
 fn main() {
-    let lines = io::read_lines("day9/input");
+    let lines = io::read_lines("day10/input");
+    let mut score = 0;
+    for line in lines {
+        let mut stack = Vec::new();
+        for c in line.chars() {
+            if ['(', '[', '{', '<'].contains(&c) {
+                stack.push(c);
+            } else {
+                let opening = match c {
+                    ')' => '(',
+                    ']' => '[',
+                    '}' => '{',
+                    '>' => '<',
+                    _ => unreachable!(),
+                };
+                if stack.pop().unwrap() != opening {
+                    let points = match c {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => unreachable!(),
+                    };
+                    score += points;
+                }
+            }
+        }
+    }
+    dbg!(score);
 }
